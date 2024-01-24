5d3217a6f39a997f88579263dbece511c1f2f71e
aykhan.akhundov <aykhan.akhundov@a2z.az>
2024-01-24 16:13:06 +0400

    build

--- modal/Coupon.js
+++ modal/Coupon.js
@@ -6,11 +6,11 @@ const mongoose = require("mongoose");
 // Dedik ki benim kategorimin bir type name ve img var,
 //
 //Men eger bunlari girersem veri tabanim da kategorysine girdim eger img girmezsem bana hata firlatmasi lazim
-const CouponSchema = mongoose.Schema({
+const CouponSchema = mongoose.Schema(
+    {
     code: {type: String, required: true}, //Burada cupon kodu yaziriq.
     discountPercent: {type: Number, required: true}, // Burada  Cuponun kodunun endirim yuzdesini yazacagiq(Indirim orani)
-   
-},
+   },
  {timestamps: true}// sececeyimiz urunun tarihini yazdiriyoruz
 
 );
